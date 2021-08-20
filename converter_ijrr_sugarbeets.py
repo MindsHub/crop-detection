6d03cd5a24d40abe4e1df37b8990778d9e446a65
Stypox <stypox@pm.me>
2021-08-20 14:12:29 +0200

    Add dataset converters

---
+++ converter_ijrr_sugarbeets.py
@@ -0,0 +1,54 @@
+import glob
+import os
+import shutil
+import cv2
+
+RGB_PATH = "datasets_raw/ijrr_sugarbeets_2016_annotations/CKA_160523/images/rgb/"
+IMAP_PATH = "datasets_raw/ijrr_sugarbeets_2016_annotations/CKA_160523/annotations/dlp/iMapCleaned/"
+COLOR_PATH = "datasets_raw/ijrr_sugarbeets_2016_annotations/CKA_160523/annotations/dlp/colorCleaned/"
+TO = "./dataset/{}/{}/"
+TYPES = {
+	"train": 1000,
+	"test": 250,
+}
+
+def getRanges(arr):
+	res = []
+	begin = arr[0]
+	last = arr[0]
+	for e in arr[1:] + [1e100]:
+		if e != last + 1:
+			res.append(str(begin) if begin == last else f"{begin}-{last}")
+			begin = e
+		last = e
+	return res
+
+def resizeImage(image):
+	image = image[0:966, 0:1288]
+	image = cv2.resize(image, (480, 360), interpolation = cv2.INTER_NEAREST)
+	return image
+
+
+files = []
+for rgbFile in sorted(glob.glob(RGB_PATH + "*.png")):
+	_, filename = os.path.split(rgbFile)
+	imapFile = IMAP_PATH + filename
+	if os.path.exists(imapFile):
+		files.append((rgbFile, imapFile, filename))
+
+print(len(files))
+
+i = 0
+for type, count in TYPES.items():
+	for j in range(count):
+		print(j)
+		rgbFile, imapFile, filename = files[i+j]
+		rgb = cv2.imread(rgbFile, cv2.IMREAD_UNCHANGED)
+		rgb = resizeImage(rgb)
+		cv2.imwrite(TO.format(type, "images") + filename, rgb)
+
+		imap = cv2.imread(imapFile, cv2.IMREAD_UNCHANGED)
+		imap = resizeImage(imap)
+		imap[imap > 0] = 1
+		cv2.imwrite(TO.format(type, "labels") + filename, imap)
+	i += count
---
+++ converter_synthetic_sugarbeat_random_weeds.py
@@ -0,0 +1,31 @@
+import glob
+import shutil
+import cv2
+import os
+
+FROM = "datasets_raw/synthetic_sugarbeat_random_weeds/gt/B_*.png"
+TO = "./dataset/{}/labels/"
+TYPES = {
+	"train": 500,
+	"test": 125,
+}
+
+imageCount = 0
+for _, count in TYPES.items():
+	imageCount += count
+
+files = glob.glob(FROM)
+if len(files) != imageCount:
+	print(f"{len(files)} files were found but the image count should be {imageCount}")
+	exit(1)
+files = sorted(files)
+
+i = 0
+for type, count in TYPES.items():
+	to = TO.format(type)
+	for j in range(count):
+		image = cv2.imread(files[i+j], cv2.IMREAD_UNCHANGED)
+		image[image > 0] = 1
+		filename = to + os.path.split(files[i+j])[1]
+		cv2.imwrite(filename, image)
+	i += count
