18646f36bfc4a0a0ba4bc0987069a6feb1595e35
Stypox <stypox@pm.me>
2021-08-20 14:14:59 +0200

    Add scripts for training

---
+++ test.py
@@ -0,0 +1,42 @@
+import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # do not use the gpu
+import keras
+import numpy as np
+import cv2
+
+INPUT_IMAGES = [
+	"../dataset/test/images/A_000504.png",
+	"../dataset/test/images/bonirob_2016-05-23-10-57-33_4_frame129.png",
+	"../dataset/test/images/cwfid_001_scaled_.png",
+	"../testing_images/x.jpg",
+	"../testing_images/y.jpg",
+	"../testing_images/z.jpg",
+]
+CHECKPOINT_PATH = "./checkpoint_352x480_dataset1"
+EPOCHS = range(1, 11)
+
+for EPOCH in EPOCHS:
+	print(f"Epoch {EPOCH}: ", end="", flush=True)
+	model = keras.models.load_model(f"{CHECKPOINT_PATH}/model_{EPOCH}.hd5")
+	outImage = np.zeros((352*4, 480 * len(INPUT_IMAGES), 3), dtype=np.uint8)
+
+	for i, path in enumerate(INPUT_IMAGES):
+		print(i, end=" ", flush=True)
+		image = cv2.imread(path)[:352, :, :]
+		out = model.predict(np.expand_dims(image, axis=0))
+
+		out1 = np.array(out[0,:,:,1] * 255, dtype=np.uint8)
+		out2 = np.array((out1 > 128) * 255, dtype=np.uint8)
+
+		erodeDilateKernel = np.ones((2, 2), np.uint8)
+		out3 = cv2.erode(out2, erodeDilateKernel, iterations=3)
+		out3 = cv2.dilate(out3, erodeDilateKernel, iterations=3)
+
+		outImage[     :352,   480*i:480*(i+1), :] = image
+		outImage[352  :352*2, 480*i:480*(i+1), :] = cv2.cvtColor(out1, cv2.COLOR_GRAY2RGB)
+		outImage[352*2:352*3, 480*i:480*(i+1), :] = cv2.cvtColor(out2, cv2.COLOR_GRAY2RGB)
+		outImage[352*3:352*4, 480*i:480*(i+1), :] = cv2.cvtColor(out3, cv2.COLOR_GRAY2RGB)
+
+	print("Writing", end=" ", flush=True)
+	cv2.imwrite(f"outImage{EPOCH}.png", outImage)
+	print("Done!")
---
+++ train.py
@@ -0,0 +1,44 @@
+import os
+#os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true' # necessary to solve some out of memory issues
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # do not use the gpu
+from unet_model import unetXceptionModel
+from sequence import CropWeedsSequence
+import keras
+import glob
+
+# config
+IMAGE_SIZE = (352, 480)
+assert IMAGE_SIZE[0] % 32 == 0
+assert IMAGE_SIZE[1] % 32 == 0
+NUM_CLASSES = 2
+CHECKPOINT_PATH = "./checkpoint_352x480_dataset2"
+
+# hyperparameters
+BATCH_SIZE = 30
+EPOCHS = 3
+LEARNING_RATE = 0.00005
+
+# calculate epochs already done and load model
+epochsAlreadyDone = sorted(glob.glob(CHECKPOINT_PATH + "/model_*.hd5"))
+if len(epochsAlreadyDone) == 0:
+	epochsAlreadyDone = 0
+	print("No epochs already done, starting from the beginning")
+	model = unetXceptionModel(IMAGE_SIZE, NUM_CLASSES)
+	model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
+else:
+	epochsAlreadyDone = int(epochsAlreadyDone[-1][len(CHECKPOINT_PATH) + 7:-4])
+	model = keras.models.load_model(CHECKPOINT_PATH + f"/model_{epochsAlreadyDone}.hd5")
+	print(f"{epochsAlreadyDone} epochs already done, resuming")
+keras.backend.set_value(model.optimizer.learning_rate, LEARNING_RATE)
+
+# saver class that saves the model with the correct name at the end of each epoch
+class CustomSaver(keras.callbacks.Callback):
+	def on_epoch_end(self, epoch, logs={}):
+		self.model.save(CHECKPOINT_PATH + f"/model_{epoch + 1}.hd5")
+os.makedirs(CHECKPOINT_PATH, exist_ok=True)
+
+# create the Sequences for loading the dataset and start training
+trainSet = CropWeedsSequence(BATCH_SIZE, IMAGE_SIZE, "./dataset/train")
+testSet = CropWeedsSequence(BATCH_SIZE, IMAGE_SIZE, "./dataset/test")
+print(f"Train set: {len(trainSet)} batches, {len(trainSet) * BATCH_SIZE} images\nTest set: {len(testSet)} batches, {len(testSet) * BATCH_SIZE} images")
+model.fit(trainSet, validation_data=testSet, initial_epoch=epochsAlreadyDone, epochs=epochsAlreadyDone+EPOCHS, callbacks=[CustomSaver()])
