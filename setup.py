9cc1ac020c51e55b17c8679c443e913083aec503
Stypox <stypox@pm.me>
2021-08-24 19:17:12 +0200

    Fix spacing in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'crop-detection', 
+    name = 'crop-detection',
     version = '1.0',
     packages = find_packages(),
-    package_dir={"": "."},
+    package_dir = {"": "."},
     package_data = {
         "": ["model*/*", "model*/*/*"]
     },
